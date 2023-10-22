e8901aab8e81da65ee3b47aea65dff5c5ab252b1
Leon <tahjib.leon399@gmail.com>
2023-10-22 10:59:43 +0600

    understanding state

--- 01ViteReact/src/App.jsx
+++ 01ViteReact/src/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
 
 function App() {
+
+  let [counter, setCounter] = useState(15);
+  function decreaseValue() {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  }
+  function increaseValue() {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  }
   return (
-    <h1>React 2.0</h1>
+    <>
+      <h1>Counter value: {counter}</h1>
+      <button onClick={increaseValue}>Increase value</button> <br />
+      <button onClick={decreaseValue}>Decrease value</button>
+    </>
   )
 }
 
 export default App
+
--- 01ViteReact/src/main.jsx
+++ 01ViteReact/src/main.jsx
@@ -1,33 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 
-// const ReactElement = {
-//   type: 'a',
-//   props: {
-//     href: 'https://github.com/leon0113/React_2.0',
-//     target: '_blank'
-//   },
-//   children: 'Click me to visit github'
-// };
-
-const user = 'Leon'
-
-const reactElement = React.createElement(
-  'a',
-  {
-    href: 'https://github.com/leon0113/React_2.0',
-    target: '_blank'
-  },
-  'Click me to visit with react ',
-  user
-)
-
-// const anotherElement = (
-//   <a href="https://github.com/leon0113" target='_blank' rel="noreferrer">Visit Me</a>
-// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  reactElement
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
 
